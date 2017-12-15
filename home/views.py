d99a51e9634f1d54b0876b77a03d1cd80ab8bc5d
clacksmith <clackken.smith@gmail.com>
2017-12-16 02:02:58 +0800

    commit new lesson

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+
+
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
 
 
 def index(request):
@@ -18,9 +19,18 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at result of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'home/results.html', {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'home/detail.html',
+                      {'question': question, 'error_message': "You didn't select a choice", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('home:results', args=(question_id,)))
